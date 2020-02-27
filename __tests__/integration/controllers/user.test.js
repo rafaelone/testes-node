f1ac6c8345a3b535139b2308bda2062a1bc021e6
Rafael Sergio <rafael.sergio@nextpage.com.br>
2020-02-27 18:20:47 -0300

    usando factory-girl e faker para gerar dados aleatorios

--- __tests__/integration/controllers/user.test.js
+++ __tests__/integration/controllers/user.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import bcrypt from 'bcryptjs';
 import app from '../../../src/app';
 import truncate from '../../util/truncate';
-import User from '../../../src/app/models/User';
+import factory from '../../factories';
 
 describe('User', () => {
   beforeEach(async () => {
@@ -10,9 +10,7 @@ describe('User', () => {
   });
 
   it('should encrypt user password when new user created', async () => {
-    const user = await User.create({
-      name: 'Rafael Sergio',
-      email: 'rafael.sergio.02@live.com',
+    const user = await factory.create('User', {
       password: '123456',
     });
     const compareHash = await bcrypt.compare('123456', user.password_hash);
@@ -20,32 +18,23 @@ describe('User', () => {
   });
 
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Rafael Sergio',
-        email: 'rafael.sergio.02@live.com',
-        password: '123456',
-      });
+      .send(user);
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
     await request(app)
       .post('/users')
-      .send({
-        name: 'Rafael Sergio',
-        email: 'rafael.sergio.02@live.com',
-        password: '123456',
-      });
+      .send(user);
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Rafael Sergio',
-        email: 'rafael.sergio.02@live.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.status).toBe(400);
   });
