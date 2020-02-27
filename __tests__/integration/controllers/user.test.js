f15895347cb73f4645c97da939d5afcaedf4f9d2
Rafael Sergio <rafael.sergio@nextpage.com.br>
2020-02-27 17:54:42 -0300

    criando teste com conceito de TDD

---
+++ __tests__/integration/controllers/user.test.js
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import app from '../../../src/app';
+
+describe('User', () => {
+  it('should be able to register', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Rafael Sergio',
+        email: 'rafael.sergio.02@live.com',
+        password_hash: '123456',
+      });
+    expect(response.body).toHaveProperty('id');
+  });
+});
