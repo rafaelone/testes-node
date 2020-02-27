f66dc818d4768e15c1c2ba94dd410de626ec1e4a
Rafael Sergio <rafael.sergio@nextpage.com.br>
2020-02-27 18:08:50 -0300

    teste para verificar se a senha é criptografada

--- __tests__/integration/controllers/user.test.js
+++ __tests__/integration/controllers/user.test.js
@@ -1,15 +1,52 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../../src/app';
+import truncate from '../../util/truncate';
+import User from '../../../src/app/models/User';
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Rafael Sergio',
+      email: 'rafael.sergio.02@live.com',
+      password: '123456',
+    });
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         name: 'Rafael Sergio',
         email: 'rafael.sergio.02@live.com',
-        password_hash: '123456',
+        password: '123456',
       });
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Rafael Sergio',
+        email: 'rafael.sergio.02@live.com',
+        password: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Rafael Sergio',
+        email: 'rafael.sergio.02@live.com',
+        password: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
